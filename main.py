355e6da5adb85e3e8d94d5242cad93c2c3f7116a
György Végh <gyorgy.vegh.job@gmail.com>
2022-06-08 15:24:25 +0200

    Refact

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def order_weight(input_list):
     arr = input_list.split()
+    # The below sort is needed because we have to order the numbers with alphabetical ordering as well. It is easier in the begining. 
     arr.sort()
+    # The ording with the requested conditions
     arr.sort(key=order_logic)
+    # In the return statement the code converts the list variable to string. 
     return ''.join(str(e)+' ' for e in arr).rstrip()
 
 def order_logic(num_str):
